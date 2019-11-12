7d04b8380fad0cc7c8b5dfc30526806f900c788b
Ankit391 <57256498+Ankit391@users.noreply.github.com>
2019-11-12 17:35:46 +0530

    Create add.cpp

---
+++ add.cpp
@@ -0,0 +1,14 @@
+#include<iostream.h>
+#include<conio.h>
+ void  main ()
+   {
+   float p,r,n;
+   cout<<"Enter the value of p :"<<endl;
+   cin>>p;
+   cout<<"Enter the value of q :"<<endl;
+   cin>>q;
+   
+   r = q+p;
+   cout<<"ADDITION is :"<<r<<endl;
+   getch();
+   }
